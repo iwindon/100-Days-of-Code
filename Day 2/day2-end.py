746c9061e05062f6e1e7cddfae67f7ccade5511b
Ivan Windon <ivan.windon@icloud.com>
2022-08-20 18:25:00 -0400

    Initial Commit

---
+++ Day 2/day2-end.py
@@ -0,0 +1,45 @@
+# num_char = len(input("What is your name: "))
+
+# new_num_char = str(num_char)
+
+# print("Your name has " + new_num_char + " characters.")
+
+# print(type(num_char))
+
+# a = str(123)
+# b = 123
+# c = float(123)
+# print(type(a))
+# print(type(b))
+# print(type(c))
+
+# print(70 + float(100.75))
+
+# two_digit_number = input("Type a two digit number: ")
+# numbers = str(two_digit_number)
+# print(int(numbers[0]) + int(numbers[1]))
+
+# print(3 * (3 + 3) / 3 - 3)
+
+
+# print("BMI Calculator")
+# height = input("enter your height in m: ")
+# weight = input("enter your weight in kg: ")
+# print(type(height))
+# print(type(weight))
+# bmi_height = float(height)
+# bmi_weight = float(weight)
+# print(type(bmi_height))
+# print(type(bmi_weight))
+# bmi = bmi_weight / bmi_height ** 2
+# print(f"Your BMI is: {int(bmi)}")
+
+
+age = input("What is your current age?")
+
+
+years = 90
+weeks = 52 * 90
+days = 365 * 90
+
+years_left = 90 - int(age)
