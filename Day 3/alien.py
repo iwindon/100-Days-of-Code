b5822fd92ec885f5d0f0e6282dc356b4af871305
Ivan Windon <ivan.windon@icloud.com>
2022-08-21 18:44:12 -0400

    Spelling corrections, ANSI art correction added additinal text to game

--- Day 3/alien.py
+++ Day 3/alien.py
@@ -10,34 +10,40 @@ print('''
            /)   (         \    ,-.    /         )   (
         ,-'"     `-.       \  /   \  /       .-'     "`-,
       ,'_._         `-.____/ /  _  \ \____.-'         _._`,
-     /,'   `.                \_/ \_/                .'   `,
-    /'       )                  _            Krogg (       `
-            /   _,-'"`-.  ,++|T|||T|++.  .-'"`-,_   
-           / ,-'        \/|`|`|`|'|'|'|\/        `-, 
-          /,'             | | | | | | |             `,
-         /'               ` | | | | | '               `
+     /,'   `.                \_/ \_/                .'   `,\\
+    /'       )                  _            Krogg (       `\\
+            /   _,-'"`-.  ,++|T|||T|++.  .-'"`-,_   \\
+           / ,-'        \/|`|`|`|'|'|'|\/        `-, \\
+          /,'             | | | | | | |             `,\\
+         /'               ` | | | | | '               `\\
                             ` | | | '
                               ` | '
 
 ''')
 print("Welcome to Alien Escape")
 print("Your mission is escape the space station.") 
+input("Press ENTER when you are ready...")
 
-print("You wake up to find yourself on a dark space craft that has been boarded by a hostile alien")
-print("You need to find your way to the escape pod as fast as possible.")
+os.system('clear')
+print("You wake up to find yourself on a dark space station that has been boarded by a hostile alien")
+print("The alarm klaxons are ringing through the ship, with an automated message repeating...")
+print("ALERT, ALERT: Abandon ship, intruder alert!")
+print("You need to find your way to the escape pod as fast as possible!")
+print("")
 print("")
 choice1 = str(input("You head down the hallway and come to a junction, do you turn left or right. (left / right): "))
 if (choice1 == "left"):
     os.system('clear')
-    choice2 = input("You walk down the cold dark hallway until you reach the elevator.  Do you take the stairs or wait for the elavator? (stairs / wait): ")
+    choice2 = input("You walk down the cold dark hallway until you reach the elevator bay. \nDo you take the stairs or wait for the elevator? (stairs / wait): ")
     if choice2 == "stairs":
         os.system('clear')
-        print("You run up 30 flights of stairs, the adrenealine pupping in your system, knowing you are getting closer to escaping.")
-        choice3 = input("You arive at the escape pod deck and are confronted with 3 doors, which pod do you want (1 / 2 / 3): ")
+        print("You run up 30 flights of stairs, the adrenealine pumping in your system, knowing you are getting closer to escaping.")
+        choice3 = input("You arive at the escape pod deck and are confronted with 3 escape doors, which pod do you want (1 / 2 / 3): ")
         if choice3 == "1":
             os.system('clear')
             print("You jump into POD 1, press the emergency launch button.")
             print("The POD bay doors close, and you are launched safely into space.")
+            print("Congratulations on making it off the space station alive")
         elif choice3 == "2":
             os.system('clear')
             print("You jump into POD 2, press the emergency launch button, yet nothing happens!")
@@ -46,11 +52,11 @@ if (choice1 == "left"):
         else:
             os.system('clear')
             print("You jump into POD 3, press the emergency launch button.")
-            print("The launch system has a catastrophic malfunction and your pod is destroyed in a ball of plasma.")
+            print("The launch system has a catastrophic malfunction and your POD is destroyed in a ball of plasma.")
             print("Game Over")
     else:
         os.system('clear')
-        print("While waiting for the slow elevator, the alien catches up from you and kills you.")
+        print("While waiting for the slow elevator, the alien catches up to you and kills you.")
         print("Game Over")
 else:
     os.system('clear')
