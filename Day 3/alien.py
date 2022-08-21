80a4ff7b1ff462d80e88ad7fa840372a8b4c90fe
Ivan Windon <ivan.windon@icloud.com>
2022-08-21 15:53:29 -0400

    Day 3 of challenge

---
+++ Day 3/alien.py
@@ -0,0 +1,27 @@
+print('''
+
+               \.   \.      __,-"-.__      ./   ./
+           \.   \`.  \`.-'"" _,="=._ ""`-.'/  .'/   ./
+            \`.  \_`-''      _,="=._      ``-'_/  .'/
+             \ `-',-._   _.  _,="=._  ,_   _.-,`-' /
+          \. /`,-',-._"""  \ _,="=._ /  """_.-,`-,'\ ./
+           \`-'  /    `-._  "       "  _.-'    \  `-'/
+           /)   (         \    ,-.    /         )   (
+        ,-'"     `-.       \  /   \  /       .-'     "`-,
+      ,'_._         `-.____/ /  _  \ \____.-'         _._`,
+     /,'   `.                \_/ \_/                .'   `,
+    /'       )                  _            Krogg (       `
+            /   _,-'"`-.  ,++|T|||T|++.  .-'"`-,_   
+           / ,-'        \/|`|`|`|'|'|'|\/        `-, 
+          /,'             | | | | | | |             `,
+         /'               ` | | | | | '               `
+                            ` | | | '
+                              ` | '
+
+''')
+print("Welcome to Alien Escape")
+print("Your mission is escape the space station.") 
+
+#https://www.draw.io/?lightbox=1&highlight=0000ff&edit=_blank&layers=1&nav=1&title=Treasure%20Island%20Conditional.drawio#Uhttps%3A%2F%2Fdrive.google.com%2Fuc%3Fid%3D1oDe4ehjWZipYRsVfeAx2HyB7LCQ8_Fvi%26export%3Ddownload
+
+#Write your code below this line 👇
