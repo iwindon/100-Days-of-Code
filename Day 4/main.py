7a37b7d2a109a4cdb99add4929279f99af9be645
Ivan Windon <ivan.windon@icloud.com>
2022-08-22 12:01:12 -0400

    Day 4 lessons

---
+++ Day 4/main.py
@@ -0,0 +1,22 @@
+# import random
+
+# random_integer = random.randint(1, 4)
+# print(random_integer)
+# random_float = random.random()
+# print(random_float)
+
+# rand_num = random.random() * 5
+# print(rand_num)
+
+# love_score = random.randint(1,100)
+# print(f"Your love score is {love_score}")
+
+states_of_america = ["Delaware", "Pennsylvania", "North Carolina", "South Carolina", "New York"]
+
+print(states_of_america[1])
+
+fruits = ["Strawberries", "Nectarines", "Apples", "Grapes", "Peaches", "Cherries", "Pears"]
+vegetables = ["Spinich", "Kales", "Tomatoes", "Celery", "Potatoes"]
+dirty_dozen = [fruits, vegetables]
+
+print(dirty_dozen)
---
+++ Day 4/map.py
@@ -0,0 +1,27 @@
+# 🚨 Don't change the code below 👇
+from pdb import post_mortem
+
+
+row1 = ["⬜️","️⬜️","️⬜️"]
+row2 = ["⬜️","⬜️","️⬜️"]
+row3 = ["⬜️️","⬜️️","⬜️️"]
+map = [row1, row2, row3]
+print(f"{row1}\n{row2}\n{row3}")
+position = input("Where do you want to put the treasure? ")
+# 🚨 Don't change the code above 👆
+
+#Write your code below this row 👇
+
+vertical = int(position[0]) -1
+horizontal = int(position[1]) -1
+
+map[vertical][horizontal] = "X"
+
+
+
+
+
+#Write your code above this row 👆
+
+# 🚨 Don't change the code below 👇
+print(f"{row1}\n{row2}\n{row3}")
