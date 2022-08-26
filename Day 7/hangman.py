a15fabf6e41eeecf8e3d77a264a19961b30cc8f6
Ivan Windon <ivan.windon@icloud.com>
2022-08-26 10:59:50 -0400

    Code cleanup and adding additinal comments

--- Day 7/hangman.py
+++ Day 7/hangman.py
@@ -3,9 +3,11 @@ import random
 from hangman_art import stages
 from hangman_art import logo
 from hangman_words import word_list
+import os
 
 chosen_word = random.choice(word_list)
 
+# starting number of life's
 lives = 6
 
 print(logo)
@@ -16,14 +18,17 @@ while num > 0:
     display.append("_")
     num -= 1
 
+# below code printed the array, it was changed to print it out as string instead
 # for _ in range(len(chosen_word)):
 #     display += "_"
 print(f"{' '.join(display)}")
 
-
+# start of game logic loop
 end_of_game = False
 while not end_of_game:
     guess = input("Guess a letter: ").lower()
+
+    os.system('clear')
 
     if guess in display:
         print(f"You already guessed the letter {guess}")
@@ -33,6 +38,7 @@ while not end_of_game:
     for letter in chosen_word:
         if letter == guess:
             display[index] = guess
+        # first attempt to lose a life found this method would not work
         # else:
         #     if guess not in chosen_word:
         #         print(stages[lives])
@@ -42,6 +48,7 @@ while not end_of_game:
 
         index += 1
 
+    # another way of checking if you guessed the correct letter
     # for position in range(len(chosen_word)):
     #     letter = chosen_word[position]
     #     if letter == guess:
@@ -77,8 +84,10 @@ while not end_of_game:
     #Join all the elements in the list and turn it into a String.
     print(f"{' '.join(display)}")
 
+    # check to see if all blanks are gone yet, if they are, end the game
     if "_" not in display:
         end_of_game = True
         print("You win!")
     
+    # printing of each stage of the game.
     print(stages[lives])
